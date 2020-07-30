6d626b1a6527c1013bb36e5cbfc28d06611f5b41
amita125 <amitaghale@gmail.com>
2020-07-30 18:41:59 +0100

    Updates

---
+++ precache-manifest.434fea990a246659b1b74c8591855a2b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a809f55d93ab2114e45250ddcdb8c0a9",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "dd509a93e6d1edbf9c8e",
+    "url": "/my-portfolio/static/css/main.efb49958.chunk.css"
+  },
+  {
+    "revision": "7fb5c8ab3c518b771916",
+    "url": "/my-portfolio/static/js/2.a1324137.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-portfolio/static/js/2.a1324137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd509a93e6d1edbf9c8e",
+    "url": "/my-portfolio/static/js/main.76491a48.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "c0caa2e59b2c863752945a7075047933",
+    "url": "/my-portfolio/static/media/Global.c0caa2e5.PNG"
+  },
+  {
+    "revision": "e162e6b5494245c27b1791d27f973073",
+    "url": "/my-portfolio/static/media/amitacv.e162e6b5.pdf"
+  },
+  {
+    "revision": "da40dedddab9df85347702dc8ed571e3",
+    "url": "/my-portfolio/static/media/chirp.da40dedd.PNG"
+  },
+  {
+    "revision": "e6e0de21603f1e1e0b8ddeac70527bda",
+    "url": "/my-portfolio/static/media/comment.e6e0de21.PNG"
+  },
+  {
+    "revision": "a4ef62a72f572034c9e13eb99ae99655",
+    "url": "/my-portfolio/static/media/dashboard.a4ef62a7.PNG"
+  },
+  {
+    "revision": "a58b8b437a798591c9ad079fa8bfdee1",
+    "url": "/my-portfolio/static/media/flower.a58b8b43.jpg"
+  },
+  {
+    "revision": "bb1e814b9f8251de1114d3f5eb0cfa93",
+    "url": "/my-portfolio/static/media/formal.bb1e814b.svg"
+  },
+  {
+    "revision": "9e152dc5a742db795d84735ebfc2d66c",
+    "url": "/my-portfolio/static/media/gallery.9e152dc5.PNG"
+  },
+  {
+    "revision": "dd8cb29e7a8245253f3d52c76d18646b",
+    "url": "/my-portfolio/static/media/gif.dd8cb29e.PNG"
+  },
+  {
+    "revision": "a112a001d5170d13d5f46d9387e526a4",
+    "url": "/my-portfolio/static/media/github.a112a001.svg"
+  },
+  {
+    "revision": "a37929fbaf1791c679e691b731ee5b22",
+    "url": "/my-portfolio/static/media/kathmandu.a37929fb.PNG"
+  },
+  {
+    "revision": "3c8afea5ee97ad99924808a2ab5fec10",
+    "url": "/my-portfolio/static/media/linkedin.3c8afea5.svg"
+  },
+  {
+    "revision": "a4b5e421a38886b484e4e834b6d0e188",
+    "url": "/my-portfolio/static/media/login.a4b5e421.PNG"
+  },
+  {
+    "revision": "5cdf96ad001e56dec5ab6c357bc16791",
+    "url": "/my-portfolio/static/media/loginpg.5cdf96ad.PNG"
+  },
+  {
+    "revision": "75d37c94a9df866ea1da28dcf94075f5",
+    "url": "/my-portfolio/static/media/mail.75d37c94.svg"
+  },
+  {
+    "revision": "45a4d5b1e4bcfdc2b929240d3d58eb4a",
+    "url": "/my-portfolio/static/media/mainPG.45a4d5b1.PNG"
+  },
+  {
+    "revision": "a9559ef2452a8edfc450acae3bb71145",
+    "url": "/my-portfolio/static/media/mainpg.a9559ef2.PNG"
+  },
+  {
+    "revision": "f776e3984564bad556f0379ef914b192",
+    "url": "/my-portfolio/static/media/map.f776e398.PNG"
+  },
+  {
+    "revision": "c182928956d873e6dcf11c082291efa1",
+    "url": "/my-portfolio/static/media/monitor.c1829289.svg"
+  },
+  {
+    "revision": "7210835c40194209de5d698dfa314501",
+    "url": "/my-portfolio/static/media/over.7210835c.PNG"
+  },
+  {
+    "revision": "69908cc2cb9e2525c7b4730f2107ecd5",
+    "url": "/my-portfolio/static/media/profile.69908cc2.PNG"
+  },
+  {
+    "revision": "7606cf040fc507d7546586a8c9fa6060",
+    "url": "/my-portfolio/static/media/question.7606cf04.PNG"
+  },
+  {
+    "revision": "3131ae5e91caddf97d55d93a30cd8914",
+    "url": "/my-portfolio/static/media/register.3131ae5e.PNG"
+  },
+  {
+    "revision": "f39096b1fe4c3f84992274d688ef64bf",
+    "url": "/my-portfolio/static/media/register.f39096b1.PNG"
+  },
+  {
+    "revision": "5676b324848c499eda98aa20f12eed46",
+    "url": "/my-portfolio/static/media/settingForm.5676b324.PNG"
+  },
+  {
+    "revision": "f0b5a50a6fd062582617351509b6f5cc",
+    "url": "/my-portfolio/static/media/tokyo.f0b5a50a.PNG"
+  },
+  {
+    "revision": "2bbba8be0581896b45b2e11296070f81",
+    "url": "/my-portfolio/static/media/weather-error.2bbba8be.png"
+  }
+]);
